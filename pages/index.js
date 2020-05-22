12667a5186e04a420b991a7206646e7e75e59f25
Ryan Bahan <ryan@ryanbahan.com>
2020-05-22 17:53:20 -0600

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,34 @@
 import Head from 'next/head'
+import useSWR, { mutate } from 'swr'
+import React, { useState } from 'react'
+
+const fetcher = () => fetch("https://api.adviceslip.com/advice").then(r => r.json());
 
 export default function Home() {
+  const { data, error } = useSWR('/api/user', fetcher);
+  const [ items, setItem ] = useState([{text: "hello"}]);
+
+  function testEvent() {
+    fetcher().then(data => {
+      setItem([...items, {text: data.slip.advice}])
+    })
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {data ? data.slip.advice : null}
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {items.map(item => <h1>{item.text}</h1>)}
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={testEvent}>test</button>
       </main>
 
       <footer>
